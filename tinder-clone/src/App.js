5664947d56f213801f3235670c597c1bdd71ec5a
Dinesh Singaria <dineshsingaria.111@gmail.com>
2023-03-12 19:44:46 +0530

    tinder clone with cardDB

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./Header.js";
+import TinderCards from "./TinderCards.js";
+import SwipeButtons from "./SwipeButtons.js";
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <TinderCards />
+      <SwipeButtons />
+    </div>
+  );
+}
+
+export default App;
